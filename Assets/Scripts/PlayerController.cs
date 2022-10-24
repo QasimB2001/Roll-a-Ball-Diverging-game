5d5c24ab6f68f6e8baff8961bc265391a2a98d73
QasimB2001 <QasimB2001@hotmail.co.uk>
2022-10-24 19:11:00 +0100

    Added Pickups
    Created pickups as prefabs and added collision detection (trigger) to them

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour {
 
     public Vector2 moveValue;
     public float speed;
+    private int count;
+    
+    void Start() {
+        count = 0;
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
@@ -14,7 +19,14 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate() {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-        
+
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count += 1;
+        }
     }
 }
